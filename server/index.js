0edf98834b98ae31f717706c06a7056398023344
Yogesh <myogeshchavan97@gmail.com>
2020-11-12 14:29:25 +0530

    added client-side routing support for express server

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(express.static(path.join(__dirname, '..', 'build')));
 app.use(fileRoute);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
+
 app.listen(3030, () => {
   console.log('server started on port 3030');
 });
